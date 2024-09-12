797f75e412bdb1927090675ecc0538d4d96252f5
Afonso_Gomes_Git <afonso.gomes.602@hotmail.com>
2024-09-12 18:06:46 -0300

    versao_atual

--- Y_millenials_na_web/millenials.js
+++ Y_millenials_na_web/millenials.js
@@ -1,5 +1,3 @@
 let nome=prompt('Bem vindo! Digite seu nome');
 let novoTexto=document.querySelector('.boas-vindas');
 novoTexto.innerHTML=`Olá ${nome}! Como está? :)`;
-
-//código javascript para interação do site
